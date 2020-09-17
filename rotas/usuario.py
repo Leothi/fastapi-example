c9e43baffcc5d11d163543b6a5007b62b95a4b8b
Leothi <leohb2@gmail.com>
2020-09-17 17:07:05 -0300

    docstring usuario

--- rotas/usuario.py
+++ rotas/usuario.py
@@ -10,6 +10,7 @@ router = APIRouter()
 # response_model é o modelo pydantic utilizado
 @router.post('/add', response_model=UserResponse, status_code=200, summary="Cria um novo usuário.")
 def add_user(dados_usuario: UserRequest) -> dict:
+    """Adiciona um usuário pra teste."""
     novo_usuario = modulo_usuario.User(**dados_usuario.dict())
     atributos = novo_usuario.user_attributes()
     return {"usuario": atributos}
