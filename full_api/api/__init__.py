243da9d3627d5638dd79e10864373080ab18e8be
Leothi <leohb2@gmail.com>
2020-12-30 16:22:44 -0300

    correção volume compose

--- full_api/api/__init__.py
+++ full_api/api/__init__.py
@@ -11,7 +11,7 @@ from api.modules.default.middleware import Middleware
 from api.exceptions import ExceptionHandler
 from api.settings import envs
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 # Configuração do Logger
 logger.configure(
